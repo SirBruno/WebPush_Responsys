31249a7016f7b753391cd8dd00564092e693cc6b
bruno pereira <bruno.pereira@compasso.com.br>
2020-10-29 14:16:52 -0300

    Added the Push Tag code

---
+++ webpush-service-worker.js
@@ -0,0 +1,3 @@
+importScripts('https://api.pushio.com/webpush/sdk/service_min.js');
+var config = '{"appserviceKey":"BFyb7Cw1IfkDLiEA_yBataZ-V2b1-y3rKwrCStDKIWgGRLB9POO4OhBGPAxuoHVCPUtU9POhorKpBqwS6PrsSZQ=","apiKey":"ABEpZmkMRtcxJnB9y-jkfOmuA","accountToken":"ABEuPNt0vy540fiOiyn5vrQ0A","appver":"0.0.0","apiHost":"https://api.pushio.com","lazy":false}';
+orawp.init(config);
